57db31958483f06dda4985410600e2be89e938b4
mabdullahjs <mabdullah2037@outlook.com>
2024-08-03 22:07:41 +0500

    javascript started

---
+++ Javascript/01-Introduction/app.js
@@ -0,0 +1,121 @@
+// console.log('hello world');
+
+
+//Data types
+
+
+//string "asdasjkd@!#!@#!6%^$&$34234" ''
+//number 20 19 10.324
+//boolean true | false
+
+
+//object
+
+
+
+
+
+
+
+
+// ()
+
+
+// console.log("abdullah")
+// console.log(20);
+// console.log(true);
+
+
+// console.log(typeof("abdullah"))
+// console.log(typeof(20))
+// console.log(typeof(true))
+
+// var rules
+
+// 123username x
+// @#username x
+// user name x
+// username 
+
+// var $_fullName = "abdullah"
+// var num = 20;
+// var isLoggedIn = true
+// console.log(username)
+// console.log(num);
+// console.log(isLoggedIn);
+
+
+// var username = "abdullah";
+// alert(username)
+
+
+// do strings ka darmiyaan + lag jaye to usko concatination
+
+// var num1 = "20";
+// var num2 = "20";
+
+// console.log(typeof(num1))
+// console.log(num1 + num2)
+
+
+
+
+// var greetUser = "hello abdullah";
+// console.log(greetUser)
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var username = "abdullah"; //declare assign initialize
+// var username = "rameez" // redeclare
+// username = "usman" // reassign
+
+
+// var hello;
+// console.log(hello)
+
+
+
+// var weight = 89
+// console.log(weight + "10");
+
+
+
+
+
+// alert("hello world");
+
+// var username = prompt("enter your name");
+// console.log(username);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
