d2d1f6e9588c2b767720029edaf3e2a5f939b363
mabdullahjs <mabdullah2037@outlook.com>
2024-08-08 16:44:34 +0500

    conditional statement

---
+++ Javascript/02-Operation-&-Expressions/app.js
@@ -0,0 +1,165 @@
+// string
+// number
+// boolean
+
+// object
+
+// "12true!@#@#$%fdjghdfkjghjkf"
+// 123
+// true | false
+
+// var abc = "abc"
+
+// console.log("hello world" , 123)
+// console.log(123)
+
+
+// var username = "abdullah";
+// var age = 21;
+
+// // my name is abdullah and my age is 21
+
+
+// console.log("my name is " + username + " and my age is " + age)
+
+
+
+// var username = prompt("enter your name");
+// console.log(username);
+
+
+
+
+
+
+
+
+
+
+
+
+//Arithmatic Operator
+
+// +
+// -
+// *
+// /
+// % modulus
+
+
+// var num1 = 20;
+// var num2 = 5;
+// console.log(num1 + num2);
+// console.log(num1 - num2);
+// console.log(num1 * num2);
+// console.log(num1 / num2);
+// console.log(num1 % num2);
+
+
+
+
+
+//Assignment Operator
+// =
+// +=
+// -=
+// /=
+// *=
+
+
+
+
+
+// var city = "kiraanchi";
+// console.log(city);
+
+// var city = "lohooor" //redeclare
+// city = "pindi" // reassign
+// console.log(city);
+
+// var num = 10;
+// num = num + 20; 
+// num = num + 20;
+// console.log(num);
+
+
+
+
+//Comparision Operator
+
+// > greater than
+// < less than
+// == data type check nahi krta 
+// === data type check krta ha
+// != not equal to data type check nahi krta
+// !== not equal to data type check krta ha
+// >= greater than equal to
+// <= less than equal to
+
+// console.log(10 > 5);
+// console.log(10 < 5);
+// console.log(10 <= 10);
+// console.log(10 >= 10);
+
+// console.log(10 == "10");
+// console.log(10 === "10");
+
+
+// console.log(10 != "10");
+// console.log(10 !== "10");
+
+
+
+
+//logical Operator
+
+//&& eng = and  urdu = or  dono condition true hona chahyaa 
+//|| eng = OR urdu Yaa aik condition true hona laazmi ha
+
+
+// console.log(10 > 10 && 10 < 20);
+// false && true
+// false
+
+
+
+// console.log(20 > 10 || 20 < 10 && 20 != "20"); //
+
+// // true || false && false
+// // true || true
+
+
+
+
+
+// false || false
+
+
+
+
+// true || false
+// true
+
+
+
+// true && true
+//true
+
+
+// true && false
+// false
+
+
+//false && false
+
+
+
+
+
+
+
+
+
+
+
+
