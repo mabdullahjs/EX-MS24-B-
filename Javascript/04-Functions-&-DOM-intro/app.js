659d3de0a12e8aad9654fb681b946cb89e76664c
mabdullahjs <mabdullah2037@outlook.com>
2024-08-14 20:10:54 +0500

    function and dom intro

--- Javascript/04-Functions-&-DOM-intro/app.js
+++ Javascript/04-Functions-&-DOM-intro/app.js
@@ -27,8 +27,147 @@
 
 
 
-function sum(num1 , num2){
-    console.log(num1 + num2);
+// function greetUser (username){
+//     console.log("hello " + username);
+// }
+
+// greetUser("abdullah")
+// greetUser("usman")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function sum(num1 , num2){
+//     console.log(+num1 + +num2);
+// }
+
+// sum("20" , "20")
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function greetUser (username){
+//     console.log("hello " + username);
+//     return "Abc"
+// }
+// //hello kuch bhi nahi
+// console.log(greetUser("a"));
+
+
+// var username = prompt("enter your name");
+// console.log(username);
+
+
+// var greet = 20;
+// console.log(greet);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function addition(num1, num2) {
+    return num1 + num2
 }
 
-sum(10 , 20)
+
+
+// var result = addition(20, 30);
+
+// console.log(result);
+
+
+
+// function banana haa us function ka parameter ka undar chem marks, phy marks, com marks, eng marks return kiya horha ho percentage
+
+
+
+// function calculatePercentage(num1, num2, num3, num4){
+//     var obtainedMarks = num1 + num2 + num3 + num4
+//     var percentage = obtainedMarks / 400 * 100;
+//     return percentage
+// }
+
+
+// console.log(calculatePercentage(30, 20, 70, 100));
+
+
+
+
+
+
+// var head = document.querySelector("#head");
+// console.log(head.innerHTML);
+
+// head.innerHTML = "changed through javascript"
+
+
+
+var para = document.querySelector("#para");
+var div = document.querySelector("div");
+
+// para.innerHTML = "Lorem ipsum, <br/> dolor sit amet consectetur adipisicing elit. Consectetur, harum."
+
+
+
+// div.innerHTML = "<h1>hello world</h1> <p>Lorem ipsum, <br/> dolor sit amet consectetur adipisicing elit. Consectetur, harum.</p> <ul><li>home</li><li>about</li><li>contact</li></ul>"
+
+
+
+
+var para = document.querySelector("#para");
+
+
+
+function changePara() {
+    if (para.innerHTML === "hello world") {
+        para.innerHTML = "hello para"
+    } else {
+        para.innerHTML = "hello world"
+    }
+
+}
+
+
+
+
+
