8e164aaa20b302a1d505f3ef400860b2b822ebb6
mabdullahjs <mabdullah2037@outlook.com>
2024-08-24 17:15:47 +0500

    loops

---
+++ Javascript/07-Loops/app.js
@@ -0,0 +1,131 @@
+
+// console.log('console before loop');
+
+
+//for loop
+//while loop
+//do while loop
+
+// initialization
+// condition
+// increment
+
+// for(var i = 1; i <= 10; i++){
+//     console.log("hello world!");
+// }
+// console.log('console after loop');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var num = 52;
+
+// for(var i = 1 ; i <= 100; i++){
+//     console.log(num +" * " + i + " = " + num * i);
+    
+// }
+
+
+
+
+// for(var i = 1; i <= 10; i++){
+//     console.log("outer loop"  , i);
+
+//     for(var j = 1; j <= 10; j++){
+//         console.log(i + " * " + j + " = " + i*j)
+//         // console.log("inner loop j" , j , i );
+        
+//     }
+
+// }
+
+
+
+
+
+const arr = ["apple" , "orange" , "watermelon" , "banana"];
+
+// console.log(arr[4]);
+
+// dom manipulation
+
+// var div = document.querySelector("div");
+
+// for(var i = 0; i < arr.length; i++){
+//     console.log(arr[i]);
+//     div.innerHTML += arr[i] + "<br/>"
+    
+// }
+
+// var num = 5
+// // num = num + 10
+// num += 10
+
+// console.log(num);
+
+
+
+//while loop
+
+// var i = 10;
+// while (i < 10){
+//     console.log(i);
+//     i++  
+// }
+
+
+
+
+// do while loop
+
+// var i = 10;
+// do {
+//     console.log(i);
+//     i++
+// } while (i < 10)
+
+
+
+
+
+
+// dom manipulation
+// loop ka through array render krwana
+// splice
+// push
+
+
+
+
+
+var username = "abdullah";
+var age = 21;
+
+console.log("your name is " + username + " and your age is " + age);
+
+console.log(`your name is ${username} and my age is ${age}`);
+
+
+// console.log("hello
+    
+//     world");
+
+// console.log(`hello 
+    
+//     world`);
+
+
+
+
+
+//ES6  
