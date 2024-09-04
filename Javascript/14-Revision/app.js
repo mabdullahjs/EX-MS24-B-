6a19d48e74db5394680be2518f64e8b270944cf4
mabdullahjs <mabdullah2037@outlook.com>
2024-09-04 20:24:43 +0500

    array loop

---
+++ Javascript/14-Revision/app.js
@@ -0,0 +1,76 @@
+// console.log("hello world!");
+
+
+// const arr = ["mango" , "orange" , "apple" , "banana"];
+
+// const obj = {
+//     username: "mabdullah",
+//     fullname: "Muhammad Abdullah",
+//     email: "mabdullah2037@gmail.com",
+//     isLoggedIn: true,
+//     hobbies: ["swimming" , "eating" , "Cooking"]
+// }
+
+
+// const ul = document.querySelector("ul");
+
+// ul.innerHTML = `<li>${obj.fullname}</li>`
+// for(let i = 0; i < arr.length; i++){
+//     console.log(arr[i]);
+//     ul.innerHTML += `<li>${arr[i]}</li>`
+
+// }
+
+
+
+
+// const students = [
+//     { name: "John Doe", age: 20, course: "Computer Science" },
+//     { name: "Jane Smith", age: 22, course: "Mathematics" },
+//     { name: "Sam Johnson", age: 19, course: "Physics" },
+//     { name: "Emily Davis", age: 21, course: "Biology" },
+//     { name: "Michael Brown", age: 23, course: "Chemistry" },
+//     { name: "Sarah Wilson", age: 20, course: "History" },
+//     { name: "David Lee", age: 18, course: "Engineering" },
+//     { name: "Laura Miller", age: 22, course: "Economics" },
+//     { name: "James Anderson", age: 19, course: "Philosophy" },
+//     { name: "Olivia Taylor", age: 21, course: "Art" }
+//   ];
+
+// console.log(students[1])
+// const div = document.querySelector(".container");
+
+// for (let i = 0; i < students.length; i++) {
+//     console.log(students[i])
+//     div.innerHTML += `
+//         <h1>Name: ${students[i].name}</h1>
+//         <h1>Age: ${students[i].age}</h1>
+//         <h1>Course: ${students[i].course}</h1>
+//         <hr>
+//     `
+
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Javascript/15-Array-Loops/app.js
@@ -0,0 +1,195 @@
+// console.log("Hello world!");
+
+
+// foreach
+// map
+// filter
+// reduce
+
+// callback
+
+
+const fruits = ["mango" , "orange" , "banana" , "apple" , "pineapple"];
+
+const students = [
+    { name: "John Doe", age: 20, course: "Computer Science" },
+    { name: "Jane Smith", age: 22, course: "Mathematics" },
+    { name: "Sam Johnson", age: 19, course: "Physics" },
+    { name: "Emily Davis", age: 21, course: "Biology" },
+    { name: "Michael Brown", age: 23, course: "Chemistry" },
+    { name: "Sarah Wilson", age: 20, course: "History" },
+    { name: "David Lee", age: 18, course: "Engineering" },
+    { name: "Laura Miller", age: 22, course: "Economics" },
+    { name: "James Anderson", age: 19, course: "Philosophy" },
+    { name: "Olivia Taylor", age: 21, course: "Art" }
+  ];
+
+const div = document.querySelector("div");
+
+// console.log(fruits);
+// fruits.forEach(function (item , index){
+//     // console.log(item , index , arr)
+//     div.innerHTML += item + "<br/>"
+
+// })
+
+
+// students.forEach(item =>{
+//     console.log(item);
+//     div.innerHTML += `<p>${item.name}</p>`
+    
+// })
+
+
+
+
+// const iteratedArr = fruits.forEach(item =>{
+//     console.log(item);
+//     return item
+// })
+
+// console.log(iteratedArr);
+
+
+// const iteratedArr = fruits.map(item =>{
+//     console.log(item);
+//     return item + " s"
+// })
+
+// console.log(iteratedArr);
+
+
+
+
+
+// const iterated = fruits.map((item)=>{
+//     console.log(item)
+//     return item + " hello"
+
+// })
+
+
+
+// const iterated = fruits.map(item => item + " hello");
+// console.log(iterated);
+
+
+
+// saara students screen pa render krwana hain through map, har student ka saath aik btn hoga delete kaa, jaisa hi us btn pa click kro wohi specific student delete hojaye. agar screen pa koi student na bacha to likha ajaye no student found.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// sum(20 , 30)
+
+// function sum(num1 , num2){
+//     console.log(num1 + num2);
+    
+//     return num1 + num2
+    
+    
+// }
+
+// let total = sum(20 , 50);
+// console.log(total)
+
+
+
+// var sum = function (num1 , num2){
+
+// }
+
+// sum()
+
+
+// sum(20 , 39)
+// const sum = (num1 , num2) => {
+//     console.log(num1 + num2)
+//     return num1 + num2
+// }
+
+// let total = sum(20 , 50);
+// console.log(total)
+
+
+
+// const greetUser = username => `hello ${username}`
+
+
+// const greetingUser = greetUser("abdullah");
+// console.log(greetingUser);
+
+
+
+// const sum = (num1 , num2) => num1 + num2;
+// const total = sum(100 , 200);
+// console.log(total);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
